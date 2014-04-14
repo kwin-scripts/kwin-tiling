bb853d0ecd1599d38a11fd64e42836e368f5ac62
Fabian Homborg <FHomborg@gmail.com>
2014-04-14 12:42:55 +0200

    Only keep client's _height_ when too small
    This is the only case that doesn't work

--- contents/code/tile.js
+++ contents/code/tile.js
@@ -233,9 +233,9 @@ Tile.prototype.setClientGeometry = function(client) {
 				var screenRect = util.getTilingArea(this._currentScreen, this._currentDesktop);
 				if (client.minSize.w > this.rectangle.width) {
 					if (this.rectangle.x + this.rectangle.width == screenRect.x + screenRect.width) {
-						this.rectangle.x = (screenRect.x + screenRect.width) - client.geometry.width;
+						this.rectangle.x = (screenRect.x + screenRect.width) - client.minSize.w;
 					}
-					this.rectangle.width = client.geometry.width;
+					this.rectangle.width = client.minSize.w;
 					changedRect = true;
 				}
 				if (client.minSize.h > this.rectangle.height) {
@@ -247,16 +247,16 @@ Tile.prototype.setClientGeometry = function(client) {
 				}
 				if (client.maxSize.w < this.rectangle.width) {
 					if (this.rectangle.x + this.rectangle.width == screenRect.x + screenRect.width) {
-						this.rectangle.x = (screenRect.x + screenRect.width) - client.geometry.width;
+						this.rectangle.x = (screenRect.x + screenRect.width) - client.maxSize.w;
 					}
-					this.rectangle.width = client.geometry.width;
+					this.rectangle.width = client.maxSize.w;
 					changedRect = true;
 				}
 				if (client.maxSize.h < this.rectangle.height) {
 					if (this.rectangle.y + this.rectangle.height == screenRect.y + screenRect.height) {
-						this.rectangle.y = (screenRect.y + screenRect.height) - client.geometry.height;
+						this.rectangle.y = (screenRect.y + screenRect.height) - client.maxSize.h;
 					}
-					this.rectangle.height = client.geometry.height;
+					this.rectangle.height = client.maxSize.h;
 					changedRect = true;
 				}
 				client.geometry = util.copyRect(this.rectangle);
