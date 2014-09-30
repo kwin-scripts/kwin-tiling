e101f9d1fab5830108358f96e34a64749781c286
Fabian Homborg <FHomborg@gmail.com>
2014-09-30 21:47:59 +0200

    Fix typo in intersectRect

--- contents/code/util.js
+++ contents/code/util.js
@@ -46,7 +46,7 @@ util.compareRect = function(rect1,rect2) {
 };
 
 util.intersectRect = function(rect1, rect2) {
-	if (rect1.x + rect1. width < rect2.x ||
+	if (rect1.x + rect1.width  < rect2.x ||
 		rect2.x + rect2.width  < rect1.x ||
 		rect1.y + rect1.height < rect2.y ||
 		rect2.y + rect2.height < rect1.y) {
